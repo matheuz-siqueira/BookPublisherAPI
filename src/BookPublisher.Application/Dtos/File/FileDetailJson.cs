2592ab36540ed7abf19e497eb8b8c691701934bf
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-11-02 17:33:07 -0300

    feat: implementing file upload and download

---
+++ src/BookPublisher.Application/Dtos/File/FileDetailJson.cs
@@ -0,0 +1,8 @@
+namespace BookPublisher.Application.Dtos.File;
+
+public class FileDetailJson
+{
+    public string DocumentName { get; set; }    
+    public string DocumentType { get; set; }
+    public string DocumentUrl { get; set; }
+}
